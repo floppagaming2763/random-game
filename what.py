c084506bd172f51443ec1409d195ed9443e30dfa
floppagaming2763 <123911773+floppagaming2763@users.noreply.github.com>
2023-02-02 10:06:48 -0500

    Update what.py

--- what.py
+++ what.py
@@ -1,11 +1,12 @@
 # a remake of a (bad) random number game
 import random
-
+x=1
 def has_numbers(inputString):
     return any(char.isdigit() for char in inputString)
 
 number = random.randrange(1,10)
-while True:
+while (x < 5):
+    x = x + 1
     answer = input("guess the number ")
     if has_numbers(answer) == False:
         print("thats not a number")
